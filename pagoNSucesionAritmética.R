ef257267210e51ccb4337d0a506b81d0b6dcdcd4
Francisco Alvarez Aguilera <144164652+FranciscoAlvarezAguilera@users.noreply.github.com>
2023-09-18 17:43:39 -0600

    pagoNSucesionAritmética.R

--- pagoNSucesionAritmética.R
+++ pagoNSucesionAritmética.R
@@ -1,5 +1,5 @@
 pagoNSucesionAritmética = function(saldo,meses,incremento){
-  
+  # Francisco Alvarez Aguilera
   Xn = (((2*saldo)/meses) + (meses-1)*incremento)/2
  print(
    paste0("El monto en la mensualidad ",meses," es de $",Xn)
